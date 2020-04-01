3ab12274805aec072b64a43d9cb09b6873bf05df
Pablo Kauê <54450039+kaue2004@users.noreply.github.com>
2020-04-01 10:13:17 -0300

    Sei n, so sei que é assim

---
+++ home.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/swiper.css">
+    <link rel="stylesheet" href="css/lightbox.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/animate.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="./js/jquery-1.8.3.min.js"></script>
+<script src="./js/jquery.elevatezoom.js"></script>
+ <script src="js/lightbox-plus-jquery.min.js"></script> 
+<script src="js/lightbox.min.js"></script> 
+<script src="./js/swiper.min.js"></script>  
+  <title>Mensagem!!!</title>
+</head>
+<body> 
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Nova mensagem pra você
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+      <h4 class="modal-title" id="myModalLabel">Nem a distância</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        
+      </div>
+      <div class="modal-body">
+      Meu amor, não faz muito tempo que você entrou no meu mundo, mas chegou de forma tão forte que todo esse mundo foi alterado por você. Toda a manhã você é meu primeiro pensamento, e também meu primeiro sorriso.
+
+Mas por entre toda a felicidade que você trouxe para minha vida, espreita a tristeza de não poder estar do seu lado todos os dias, pois isso é tudo o que eu mais quero.
+
+Mas apesar desta distância, eu sei que nosso relacionamento, e ainda que recente, é forte e a tudo resistirá, pois foi o amor um pelo outro que nos uniu, e é ele que nos vai manter juntos, e eu tudo vou fazer para que assim seja.
+
+Você é a menina mulher que eu amo, a pessoa com quem eu quero compartilhar a minha vida! Mas por enquanto compartilhamos esta saudade, e o ardente desejo de nos fundirmos em um apertado abraço, e de uma vez por todas destruir esta distância que nos separa. Eu te amo!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
